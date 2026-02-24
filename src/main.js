60a7420c74ea4a9cff45afcc8a824e6e220527d3
jonathanjordan2106 <jonathanjordan2106@gmail.com>
2026-02-24 13:47:21 +0700

    test: fix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { setupCounter } from './counter.js'
 
 const button = document.querySelector('#counter')
 
+console.log("test main.js")
+
 if (!button) {
   throw new Error('Start button (#counter) not found')
 }
